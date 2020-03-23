87e02ae408b55e531681e4c1e92712c2eff19ff6
iamsoto <iambriansoto@gmail.com>
2020-03-23 10:29:27 -0700

    Returning a list of links with Reuters

--- Reuters.py
+++ Reuters.py
@@ -8,30 +8,45 @@ https://morioh.com/p/abf3d53739f8
 
 https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-Wash Post target URL: 
-https://www.washingtonpost.com/politics/white-house/
+Reuters URL
+https://www.reuters.com/politics
+
+Find specific text in a web page
+https://stackoverflow.com/questions/51640636/how-to-locate-a-specific-html-row-containing-specific-text-with-python
+
 """
 
 # Test 1: Give base URL, return article links
-def get_article_links(url="https://www.washingtonpost.com/politics/white-house/"):
+"""
+	format: 
+		<h2>
+			<a> </a> 
+		</h2>
+"""
+def get_article_links(url="https://www.reuters.com/politics"):
 	r1 = requests.get(url)
 	coverpage = r1.content	
 	soup1 = BeautifulSoup(coverpage, "html.parser")
 
-	# We're looking for the class div class = "story-headline"
-	headline_divs = soup1.find_all('div', class_='story-headline')
+	# Grab all header tags
+	headline_tags = soup1.find_all('h2', class_ = 'FeedItemHeadline_headline')
+	article_urls = []
+	
+	# Grab the hyper link
+	for headline_tag in headline_tags:
+		try:
+			article_urls.append(headline_tag.a['href'])
+		except Exception as e:
+			print(e)
 
-	# This works :) 
-	links = []
-	for child in headline_divs:
-		links.append(child.find('h2').find('a')['href'])
-	return links 
+	return article_urls
 	#return headline_divs[0].find('h2').find('a')['href']
 
 
 def testing2():
 	print(str(len(get_article_links())))
 
+"""
 def testing():
 	file_name = "WashPost_Headers.txt"
 	with open(file_name, "w+")as f:
@@ -39,6 +54,7 @@ def testing():
 		for child in children_of_first_story_headline_div:
 			f.write("{}\n".format(str(child)))
 
+"""
 testing2()
 #testing()
 
